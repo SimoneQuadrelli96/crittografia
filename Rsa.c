935ca1fea480a8c858a77ac7f02200edc2d15270
Simone Quadrelli <33251259+alalai96@users.noreply.github.com>
2017-11-11 10:02:51 +0100

    Add files via upload

--- Rsa.c
+++ Rsa.c
@@ -21,16 +21,11 @@ void fermat_factorization(){
     mpz_sqrt(risultato,b2);
     mpz_sub(risultato,a,risultato);
 
-     gmp_printf("Ris %Zd\n\n",risultato);
-
-     if(mpz_cmp(risultato,n) != 0)
-      printf("Trovato\n");
-    else
-      printf("ERRORE\n");
+     gmp_printf("Risultato: %Zd\n\n",risultato);
 }
 
 
-void common_module_attack(){
+void common_modulus_attack(){
   mpz_t n,eA,eB,d,cA,cB,r,s,mcd, ris1, ris2, inv_cB;
   mpz_init(inv_cB);
   mpz_init(ris1);
@@ -45,14 +40,10 @@ void common_module_attack(){
   mpz_init_set_str(cB,"269125499927676766418170761935369755935025654357890774691167668682175704132138220498309902",10);
 
 
-  //troviamo i fttori r ed s. S è negativo
+  //troviamo i fattori r ed s. S è negativo
   mpz_gcdext(mcd,r,s,eA,eB);
-  gmp_printf("cA^r mod n %Zd\n\n",r);
-
-
   //calcolo cA^r mod n
   mpz_powm(ris1, cA,r,n);
-  gmp_printf("cA^r mod n %Zd\n\n",ris1);
 
   //poichè s è l'esponente di  cB poichè s è negativo calcolo l'inverso moltiplicativo di cB e il valore assoluto di s
   mpz_invert(inv_cB,cB,n);
@@ -60,28 +51,21 @@ void common_module_attack(){
 
   //calcolo inv_cB ^ (-s) mod n
   mpz_powm(ris2,inv_cB,s,n);
-  gmp_printf("inv_cB ^s mod n %Zd\n\n",ris2);
 
   // faccio il prodotto dei fattori ottenuti
   mpz_mul(ris1,ris1,ris2);
   mpz_mod(ris1, ris1, n);
-  gmp_printf("Risultato %Zd\n\n",ris1);
+  gmp_printf("Risultato: %Zd\n\n",ris1);
 
-
-
-char * message = malloc(sizeof(char)* 1000) ;
-  message = mpz_get_str (message,10,ris1);
-  message[999] = '\0';
-  int i = 0;
-  for(i = 0; i < 65;i= i+2){
-    printf("%c%c ",message[i], message[i+1]);
-  }
-  printf("\n");
 }
 
 
 int main(int argc, char *argv[]) {
-  //fermat_factorization();
-  common_module_attack();
+  printf("Inizio fattorizzazione di fermat \n\n");
+  fermat_factorization();
+  printf("Fine fattorizzazione di fermat \n\n");
+  printf("Inizio common modulus attack \n\n");
+  common_modulus_attack();
+  printf("Fine common modulus attack \n\n");
   return 0;
 }
